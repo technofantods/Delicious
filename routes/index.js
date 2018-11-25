cbbe1c18fdb99f7c4aeb8df35de35dde03f01d1f
Bryan Snyder <bryansnyder@pm.me>
2018-11-25 16:16:50 -0500

    index.js and .pug updated

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,7 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  console.log('Gary is fuckin TOOTHY!');
-  res.send('Baba Booey is Ta Ta Toofy!');
+   res.render('index');
 });
 
 module.exports = router;
