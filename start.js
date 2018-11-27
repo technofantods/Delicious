d1eb6419afc3910b448d24bfd92f478d6dcea932
Bryan Snyder <bryansnyder@pm.me>
2018-11-26 22:28:32 -0500

    small updates

--- start.js
+++ start.js
@@ -7,20 +7,17 @@ if (major < 7 || (major === 7 && minor <= 5)) {
   process.exit();
 }
 
-// import environmental variables from our variables.env file
+// import env variables from variables.env file
 require('dotenv').config({ path: 'variables.env' });
 
-// Connect to our Database and handle any bad connections
+// Connect to DB, handle bad connections
 mongoose.connect(process.env.DATABASE);
 mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
 mongoose.connection.on('error', (err) => {
   console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
 });
 
-// READY?! Let's go!
-
-
-// Start our app!
+// Start app
 const app = require('./app');
 app.set('port', process.env.PORT || 7777);
 const server = app.listen(app.get('port'), () => {
